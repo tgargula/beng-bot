efef9d04e49f140dfe5ed3b1a9bff8503a9cf23c
tgargula <tomaszgargula1@gmail.com>
2022-03-02 23:10:36 +0100

    express

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const http = require('http');
+const express = require("express");
+const app = express();
+const port = 8080;
 
-const requestListener = (req, res) => {
+app.use(express.json());
+
+app.post("/pr", (req, res) => {
   console.log(req.body);
+  res.send("Hello World!");
+});
 
-  res.writeHead(200);
-  res.end('Hello, World!');
-}
+app.use((req, res, next) => {
+  res.status(404).json({ error: "path not found" });
+});
 
-const server = http.createServer(requestListener);
-server.listen(8080);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
